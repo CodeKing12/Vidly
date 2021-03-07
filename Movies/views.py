ed48cd162c714e9ece00a4803bd4dbdebea22832
Kingsley <eyetukingsley330@gmail.com>
2021-03-07 17:40:01 +0100

    Initial commit

---
+++ Movies/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+
+from .models import Movie
+
+# Create your views here.
+
+def index(request):
+    movies = Movie.objects.all()
+    return render(request, template_name="Movies/index.html", context={"movies": movies})
+
+def details(request, movies_id):
+    movie = get_object_or_404(Movie, id=movies_id)
+    return render(request, "Movies/details.html", {"movie": movie})
